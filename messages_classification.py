96bb16913137dee9096af1dd52f1b85f59f5da82
antoaguilerac <97893058+antoniaaguilera@users.noreply.github.com>
2022-05-10 20:45:51 -0400

    avances

--- messages_classification.py
+++ messages_classification.py
@@ -71,10 +71,10 @@ basica_2 =  mime_all['message'].str.contains('básico|basico|básica|basica|prim
 basica_3 =  mime_all['message'].str.contains('básico|basico|básica|basica|primaria') & mime_all['message'].str.contains('3|3b|3ro|3ero|3º|3°|tercero')
 basica_4 =  mime_all['message'].str.contains('básico|basico|básica|basica|primaria') & mime_all['message'].str.contains('4|4b|4to|4º|4°|cuarto')
 
-basica_5 =  mime_all['message'].str.contains('5|5to|5º|5°|quinto')
-basica_6 =  mime_all['message'].str.contains('6|6to|6º|6°|sexto')
-basica_7 =  mime_all['message'].str.contains('7|7mo|7º|7°|septimo|séptimo')
-basica_8 =  mime_all['message'].str.contains('8|8vo|8º|8°|octavo')
+basica_5 =  mime_all['message'].str.contains('5|5to|5º|5°|quinto|5basico|5basica')
+basica_6 =  mime_all['message'].str.contains('6|6to|6º|6°|sexto|6basico|6basica')
+basica_7 =  mime_all['message'].str.contains('7|7mo|7º|7°|septimo|séptimo|7basico|7basica')
+basica_8 =  mime_all['message'].str.contains('8|8vo|8º|8°|octavo|8basico|8basica')
 
 media_1 =  mime_all['message'].str.contains('medio|media') & mime_all['message'].str.contains('1|1ro|1ero|1º|1°|primero|iº|i°|i*')
 media_2 =  mime_all['message'].str.contains('medio|media') & mime_all['message'].str.contains('2|2do|2º|2°|segundo|iiº|ii°|ii*')
@@ -111,6 +111,8 @@ for x in range(len(basica_1)) :
         mime_all['grade'][x] = "IIIº medio"
     elif media_4.loc[x] == True:
         mime_all['grade'][x] = "IVº medio"
+    else:
+        mime_all['grade'][x] = "Sin curso"
 
 
 # ---------------------------------------------------------------------- #
@@ -155,7 +157,7 @@ mime_all['lg_lista_espera'] = mime_all['message'].str.contains(lista_espera)
 #busca traslado
 mime_all['lg_traslado'] = mime_all['message'].str.contains(verb) & mime_all['message'].str.contains(traslado)
 
-#busca traslado
+#busca pie
 mime_all['lg_pie'] = mime_all['message'].str.contains(kid) & mime_all['message'].str.contains(pie)
 
 #conocer horario
@@ -225,18 +227,17 @@ mime_all['Category'] = np.where(mime_all['sp_highered']==True, 'Service Provider
 # ---------------------------------------------------------------------- #
 # -------------------------- LOOKING FOR JOB --------------------------- #
 # ---------------------------------------------------------------------- #
-teacher = 'profesor|profesora|docente|educador|educadora|equipo docente|egresado|egresada'
+teacher = 'profesor|profesora|docente|educador|educadora|equipo docente|egresado|egresada|licenciada|licenciado'
 therapy =  'fonoaudióloga|fonoaudiologa|fonoaudiólogo|fonoaudiologo|psicóloga|psicologa|psicólogo|psicologo|kinesiologo|kinesiologa|kinesiólogo|kinesióloga'
 classroom_asistance = 'psicopedagogo|psicopedagoga|auxiliar|asistente|orientador|orientadora|inspector|inspectora'
 asistentes = 'psicopedagogo|psicopedagoga|auxiliar|asistente|orientado|orientadora|inspector|inspectora|bibliotecólogo|bibliotecóloga|bibliotecologo|bibliotecologa'
 
 resume = 'cv|curriculum|curriculo|curriculum vitae|currículo|curriculo'
-job = 'trabajo|puesto de trabajo|oportunidad laboral|empleo|trabajar|integrarme a su equipo|requieren docentes'
+job = 'trabajo|puesto de trabajo|oportunidad laboral|empleo|trabajar|integrarme a su equipo|requieren docentes|vacante laboral'
 
 mime_all['jobs_teaching'] = mime_all['message'].str.contains(teacher) & (mime_all['message'].str.contains(job)|mime_all['message'].str.contains(resume))
 mime_all['jobs_therapy']  = mime_all['message'].str.contains(therapy) & (mime_all['message'].str.contains(job)|mime_all['message'].str.contains(resume))
 mime_all['jobs_asistance']  = mime_all['message'].str.contains(classroom_asistance) & (mime_all['message'].str.contains(job)|mime_all['message'].str.contains(resume))
-
 
 
 mime_all['Category'] = np.where(mime_all['jobs_teaching']==True, 'Looking for Jobs',mime_all['Category'])
@@ -246,24 +247,29 @@ mime_all['Category'] = np.where(mime_all['jobs_asistance']==True, 'Looking for J
 # ---------------------------------------------------------------------- #
 # ------------------------------ OTHERS -------------------------------- #
 # ---------------------------------------------------------------------- #
-students = 'estudiante|alumno|alumna'
+students = 'soy estudiante|soy alumno|soy alumna'
 institution = 'universidad|instituto|pedagogía'
 internship = 'práctica|práctica profesional|proyecto de título|proyecto de titulo|trabajo'
 colision = 'colision|colisión|colisionado|colisionada'
-link = 'link|web|página|pagina'
+link = 'link|página web|pagina web|web'
 #necesito el contacto
 mime_all['other_internship'] = mime_all['message'].str.contains(students) & mime_all['message'].str.contains(internship) & mime_all['message'].str.contains(institution)
 mime_all['other_colision'] = mime_all['message'].str.contains(colision)
 mime_all['other_link'] = mime_all['message'].str.contains(link)
 
-mime_all['Category'] = np.where(mime_all['other_internship']==True, 'Other',mime_all['Category'])
-mime_all['Category'] = np.where(mime_all['other_colision']==True, 'Other',mime_all['Category'])
-mime_all['Category'] = np.where(mime_all['other_link']==True, 'Other',mime_all['Category'])
+
+mime_all['Category'] = np.where(mime_all['other_internship']==True , 'Other',mime_all['Category'])
+mime_all['Category'] = np.where(mime_all['other_colision']==True , 'Other',mime_all['Category'])
+#mime_all['Category'] = np.where(mime_all['other_link']==True , 'Other',mime_all['Category'])
+
+# ---------------------------------------------------------------------- #
+# --------------------------- CORRECTIONS ------------------------------ #
+# ---------------------------------------------------------------------- #
+#mime_all['lg_busca_vacante'] = np.where(mime_all['Category']!='Parent/Legal Guardian', False ,mime_all['lg_busca_vacante'])
+
 
 # ---------------------------------------------------------------------- #
 # ----------------------------- SAVE FILE ------------------------------ #
 # ---------------------------------------------------------------------- #
-
-mime_all['lg_busca_vacante'] = np.where(mime_all['Category']!='Parent/Legal Guardian', False ,mime_all['lg_busca_vacante'])
 
 mime_all.to_excel(f'{save_path}/clean/messages_analysis.xlsx')
